3bdc80efce21cd3f494529da161be626f24f794e
TiagoMerc <tmrrosario9@gmail.com>
2022-03-18 16:22:45 -0300

    Parte 6 - Gerando loop no jogo

--- js/js.js
+++ js/js.js
@@ -11,4 +11,29 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+//Principais variáveis do jogo
+	
+var jogo = {};
+	
+//Game Loop
+
+jogo.timer = setInterval(loop,30); /*temporizador, onde indica a função loop e o tempo de 30 milissegundos */
+
+function loop() {
+
+movefundo();
+
+} // Fim da função loop()
+
+//Função que movimenta o fundo do jogo
+	
+function movefundo() {
+	
+	esquerda = parseInt($("#fundoGame").css("background-position")); /* Converte string em um número inteiro, pegando o valor atual do fundo da div*/
+	$("#fundoGame").css("background-position",esquerda-1); /*Andando um pix para a esquerda */
+	
+  /* Para o fundo passar mais rápido, mudar o valor acima "esquerda-1"*/
+	} // fim da função movefundo()
+
+
 } // Fim da função start
