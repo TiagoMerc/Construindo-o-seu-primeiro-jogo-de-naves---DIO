07a94f758283ae2227de0ae0370b7b4f30053b81
TiagoMerc <tmrrosario9@gmail.com>
2022-03-18 15:40:42 -0300

    Parte 4 - comportamento no jogo com JS

---
+++ js/js.js
@@ -0,0 +1,14 @@
+/**
+ Primeira função do jogo 
+ */
+
+function start() { // Inicio da função start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+} // Fim da função start
