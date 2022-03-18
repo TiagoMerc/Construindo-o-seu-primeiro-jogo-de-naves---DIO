ff6a41873c139e2d13e20faf47d3564d7a69a8d0
TiagoMerc <tmrrosario9@gmail.com>
2022-03-18 17:40:13 -0300

    Parte 11 - Movimentando inimigo 2 e amigo

--- js/js.js
+++ js/js.js
@@ -15,16 +15,15 @@ function start() {
   //Principais variáveis do jogo
 
   var jogo = {}
-  var velocidade=5;
-  var posicaoY = parseInt(Math.random() * 334); //O inimigo pode estar posicionado tanto no 0 quanto 334. Função Math.random
- 
+  var velocidade = 5
+  var posicaoY = parseInt(Math.random() * 334) //O inimigo pode estar posicionado tanto no 0 quanto 334. Função Math.random
+
   var TECLA = {
     /*Valor decimal de cada tecla */
     W: 87, //Movimentar o helicopteto para baixo
     S: 83, //Realizar os disparos
     D: 68 //
   }
-
 
   //Usar outras teclas, usar keycode
   jogo.pressionou = []
@@ -51,8 +50,9 @@ function start() {
   function loop() {
     movefundo()
     movejogador() //Função move jogador
-    moveinimigo1();
-
+    moveinimigo1()
+    moveinimigo2()
+    moveamigo();
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -102,17 +102,38 @@ function start() {
 
   //Função move inimigo
   function moveinimigo1() {
+    posicaoX = parseInt($('#inimigo1').css('left'))
+    $('#inimigo1').css('left', posicaoX - velocidade)
+    $('#inimigo1').css('top', posicaoY)
 
-    posicaoX = parseInt($("#inimigo1").css("left"));
-    $("#inimigo1").css("left",posicaoX-velocidade); 
-    $("#inimigo1").css("top",posicaoY);
-      
-      if (posicaoX<=0) {
-      posicaoY = parseInt(Math.random() * 334);
-      $("#inimigo1").css("left",694);
-      $("#inimigo1").css("top",posicaoY); //reposicionando 
-        
-      }
+    if (posicaoX <= 0) {
+      posicaoY = parseInt(Math.random() * 334)
+      $('#inimigo1').css('left', 694)
+      $('#inimigo1').css('top', posicaoY) //reposicionando
+    }
   } //Fim da função moveinimigo1()
 
+  //Move inimigo 2
+  function moveinimigo2() {
+    posicaoX = parseInt($('#inimigo2').css('left')) //div inimigo 2, pega a div e subtrai, caminha 3 unidas para esquerda 
+    $('#inimigo2').css('left', posicaoX - 3)
+
+    if (posicaoX <= 0) { //Tiver variável menor e igual a 0, reposiciona o inimigo 2 no lado direito da "div"   
+      $('#inimigo2').css('left', 775)
+    }
+  } // Fim da função moveinimigo2()
+
+  //Função move amigo 
+  function moveamigo() {
+	
+  posicaoX = parseInt($("#amigo").css("left")); //var numa posição que é a left, que atualiza com a  posiçãoX"
+    $("#amigo").css("left",posicaoX+1);
+          
+      if (posicaoX>906) { //volta pro 0
+        
+      $("#amigo").css("left",0); 
+            
+      }
+  
+  } // fim da função moveamigo()
 } // Fim da função start
