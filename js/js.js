4d4a2b658d85eb75b4715e0d51b62d8665d0cc52
TiagoMerc <tmrrosario9@gmail.com>
2022-03-18 21:13:21 -0300

    Parte 16 - Finalizando colisões

--- js/js.js
+++ js/js.js
@@ -250,6 +250,17 @@ function start() {
       reposicionaAmigo()
       $('#amigo').remove()
     }
+
+    //Inimigo2 com o amigo
+
+    if (colisao6.length > 0) {
+      amigoX = parseInt($('#amigo').css('left')) //posição atual da div amigo
+      amigoY = parseInt($('#amigo').css('top'))
+      explosao3(amigoX, amigoY)
+      $('#amigo').remove()
+
+      reposicionaAmigo()
+    }
   } //Fim da função colisao()
 
   //Função explosão
@@ -309,23 +320,31 @@ function start() {
   } // Fim da função explosao2()
 
   //Reposiciona Amigo
-	
-	function reposicionaAmigo() {
-	
-    var tempoAmigo=window.setInterval(reposiciona6, 6000);
-    
-      function reposiciona6() {
-      window.clearInterval(tempoAmigo);
-      tempoAmigo=null;
-      
-      if (fimdejogo==false) {
-      
-      $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
-      
+
+  function reposicionaAmigo() {
+    var tempoAmigo = window.setInterval(reposiciona6, 6000)
+
+    function reposiciona6() {
+      window.clearInterval(tempoAmigo)
+      tempoAmigo = null
+
+      if (fimdejogo == false) {
+        $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
       }
-      
     }
-    
   } // Fim da função reposicionaAmigo()
-  
+
+  //Explosão3
+
+  function explosao3(amigoX, amigoY) {
+    $('#fundoGame').append("<div id='explosao3' class='anima4'></div")
+    $('#explosao3').css('top', amigoY)
+    $('#explosao3').css('left', amigoX)
+    var tempoExplosao3 = window.setInterval(resetaExplosao3, 1000)
+    function resetaExplosao3() {
+      $('#explosao3').remove()
+      window.clearInterval(tempoExplosao3)
+      tempoExplosao3 = null
+    }
+  } // Fim da função explosao3
 } // Fim da função start
