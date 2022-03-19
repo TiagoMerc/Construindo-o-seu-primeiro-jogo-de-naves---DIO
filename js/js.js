9804b536784beadb8e35909298d7d467bd522a97
TiagoMerc <tmrrosario9@gmail.com>
2022-03-18 21:26:49 -0300

    Parte 17 - Pontuação, amigos salvos e perdidos

--- js/js.js
+++ js/js.js
@@ -11,11 +11,16 @@ function start() {
   $('#fundoGame').append("<div id='inimigo1' class='anima2'></div>")
   $('#fundoGame').append("<div id='inimigo2'></div>")
   $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
+  $("#fundoGame").append("<div id='placar'></div>");
 
   //Principais variáveis do jogo
 
   var podeAtirar = true
   var fimdejogo = false
+  var pontos=0;
+  var salvos=0;
+  var perdidos=0;
+
   var jogo = {}
   var velocidade = 5
   var posicaoY = parseInt(Math.random() * 334) //O inimigo pode estar posicionado tanto no 0 quanto 334. Função Math.random
@@ -56,6 +61,8 @@ function start() {
     moveinimigo2()
     moveamigo()
     colisao()
+    placar();
+
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -220,6 +227,9 @@ function start() {
     // Disparo com o inimigo1
 
     if (colisao3.length > 0) {
+
+      
+      pontos=pontos+100;//placar 
       inimigo1X = parseInt($('#inimigo1').css('left')) //pega as posicções do inimigo
       inimigo1Y = parseInt($('#inimigo1').css('top'))
 
@@ -234,6 +244,8 @@ function start() {
     // Disparo com o inimigo2
 
     if (colisao4.length > 0) {
+
+      pontos=pontos+50; //Placar
       inimigo2X = parseInt($('#inimigo2').css('left'))
       inimigo2Y = parseInt($('#inimigo2').css('top'))
       $('#inimigo2').remove()
@@ -247,6 +259,8 @@ function start() {
     // jogador com o amigo
 
     if (colisao5.length > 0) {
+
+      salvos++; //Placar
       reposicionaAmigo()
       $('#amigo').remove()
     }
@@ -254,6 +268,9 @@ function start() {
     //Inimigo2 com o amigo
 
     if (colisao6.length > 0) {
+
+      
+      perdidos++; //placar
       amigoX = parseInt($('#amigo').css('left')) //posição atual da div amigo
       amigoY = parseInt($('#amigo').css('top'))
       explosao3(amigoX, amigoY)
@@ -347,4 +364,13 @@ function start() {
       tempoExplosao3 = null
     }
   } // Fim da função explosao3
+
+  //Placar do jogo
+  function placar() {
+	
+    $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
+    
+  } //fim da função placar()
+
+
 } // Fim da função start
